82c4a0cb5ecc7ef0fabb50cf8f1024b7c2922ce5
Eman Omer <pg32219@umbc.edu>
2026-04-25 23:05:42 -0500

    final display with name

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { generateClient } from "aws-amplify/data";
-import { useEffect, useState } from "react";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-const client = generateClient();
-
 function App() {
   const { user, signOut } = useAuthenticator();
-  const [userProfile, setUserProfile] = useState(null);
-
-  useEffect(() => {
-    client.models.UserProfile.list().then(({ data }) => {
-      if (data.length > 0) setUserProfile(data[0]);
-    });
-  }, []);
 
   return (
     <main>
-      <h1>Hello, {user?.signInDetails?.loginId}</h1>
-      {userProfile && <p>Profile Owner: {userProfile.profileOwner}</p>}
+      <h1>Hello, Sudies Hussein</h1>
+      <p>Email: {user?.signInDetails?.loginId}</p>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
